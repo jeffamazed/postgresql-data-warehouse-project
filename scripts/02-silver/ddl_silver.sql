af672348ab55fbddc76ddb7c1a6f84559e4eb796
jeffamazed <jeffry1408.job@gmail.com>
2025-07-28 16:01:19 +0700

    feat(ddl_silver.sql): add new script for adding tables and data into silver layer schema

---
+++ scripts/02-silver/ddl_silver.sql
@@ -0,0 +1,88 @@
+-- =============================================================
+-- Drop and Recreate Tables in the 'silver' schema
+-- =============================================================
+
+-- Drop tables if they exist
+DROP TABLE IF EXISTS silver.crm_cust_info;
+DROP TABLE IF EXISTS silver.crm_prd_info;
+DROP TABLE IF EXISTS silver.crm_sales_details;
+DROP TABLE IF EXISTS silver.erp_loc_a101;
+DROP TABLE IF EXISTS silver.erp_cust_az12;
+DROP TABLE IF EXISTS silver.erp_px_cat_g1v2;
+
+-- =============================================================
+-- Create Customer Info Table
+-- =============================================================
+CREATE TABLE silver.crm_cust_info (
+  cst_id INT,
+  cst_key VARCHAR(50),
+  cst_firstname VARCHAR(50),
+  cst_lastname VARCHAR(50),
+  cst_marital_status VARCHAR(50),
+  cst_gndr VARCHAR(50),
+  cst_create_date DATE,
+  dwh_create_date TIMESTAMP DEFAULT NOW()
+);
+
+-- =============================================================
+-- Create Product Info Table
+-- =============================================================
+CREATE TABLE silver.crm_prd_info (
+  prd_id INT,
+  cat_id VARCHAR(50),
+  prd_key VARCHAR(50),
+  prd_nm VARCHAR(50),
+  prd_cost INT,
+  prd_line VARCHAR(50),
+  prd_start_dt DATE,
+  prd_end_dt DATE,
+  dwh_create_date TIMESTAMP DEFAULT NOW()
+);
+
+-- =============================================================
+-- Create Sales Details Table
+-- =============================================================
+CREATE TABLE silver.crm_sales_details (
+  sls_ord_num VARCHAR(50),
+  sls_prd_key VARCHAR(50),
+  sls_cust_id INT,
+  sls_order_dt DATE,
+  sls_ship_dt DATE,
+  sls_due_dt DATE,
+  sls_sales INT,
+  sls_quantity INT,
+  sls_price INT,
+  dwh_create_date TIMESTAMP DEFAULT NOW()
+);
+
+-- =============================================================
+-- Create Location Info Table
+-- =============================================================
+CREATE TABLE silver.erp_loc_a101 (
+  cid VARCHAR(50),
+  cntry VARCHAR(30), 
+  dwh_create_date TIMESTAMP DEFAULT NOW()
+);
+
+-- =============================================================
+-- Create ERP Customer Info Table
+-- ============================ =================================
+CREATE TABLE silver.erp_cust_az12 (
+  cid VARCHAR(50),
+  bdate DATE,
+  gen VARCHAR(10),
+  dwh_create_date TIMESTAMP DEFAULT NOW()
+);
+
+-- =============================================================
+-- Create Product Category Mapping Table
+-- =============================================================
+CREATE TABLE silver.erp_px_cat_g1v2 (
+  id VARCHAR(20),
+  cat VARCHAR(20),
+  subcat VARCHAR(40),
+  maintenance VARCHAR(10),
+  dwh_create_date TIMESTAMP DEFAULT NOW()
+);
+
+
